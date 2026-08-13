1dc108fddf4fa29b02720859996a8b64867e6c2a
Lucas <devlucaznery@gmail.com>
2026-08-13 16:56:40 -0300

    feat: wire free player movement, Assaltante chase, and camera follow into ArenaScene

--- src/scenes/ArenaScene.ts
+++ src/scenes/ArenaScene.ts
@@ -3,6 +3,7 @@ import Phaser from 'phaser';
 import { Encounter } from '../combat/encounter';
 import { OpportunityOverlay } from '../debug/opportunityOverlay';
 import { createFixedTimestepLoop } from '../core/fixedTimestepLoop';
+import { ARENA_BOUNDS } from '../combat/movementDefs';
 
 const STEP_MS = 1000 / 60;
 
@@ -12,7 +13,14 @@ export class ArenaScene extends Phaser.Scene {
   private overlayText!: Phaser.GameObjects.Text;
   private playerRect!: Phaser.GameObjects.Rectangle;
   private assaltanteRect!: Phaser.GameObjects.Rectangle;
-  private keys!: { light: Phaser.Input.Keyboard.Key; dodge: Phaser.Input.Keyboard.Key };
+  private keys!: {
+    light: Phaser.Input.Keyboard.Key;
+    dodge: Phaser.Input.Keyboard.Key;
+    up: Phaser.Input.Keyboard.Key;
+    down: Phaser.Input.Keyboard.Key;
+    left: Phaser.Input.Keyboard.Key;
+    right: Phaser.Input.Keyboard.Key;
+  };
 
   constructor() {
     super('ArenaScene');
@@ -21,18 +29,18 @@ export class ArenaScene extends Phaser.Scene {
   create(): void {
     this.encounter = new Encounter(
       { x: 100, y: 300, width: 20, height: 20 },
-      { x: 130, y: 300, width: 20, height: 20 },
-      30,
+      { x: 400, y: 300, width: 20, height: 20 },
     );
 
-    this.playerRect = this.add.rectangle(100, 300, 20, 20, 0x4caf50);
-    this.assaltanteRect = this.add.rectangle(130, 300, 20, 20, 0xf44336);
+    this.playerRect = this.add.rectangle(100, 300, 20, 20, 0x4caf50).setOrigin(0, 0);
+    this.assaltanteRect = this.add.rectangle(400, 300, 20, 20, 0xf44336).setOrigin(0, 0);
 
     this.overlayText = this.add.text(10, 10, '', {
       fontFamily: 'monospace',
       fontSize: '16px',
       color: '#ffffff',
     });
+    this.overlayText.setScrollFactor(0);
     new OpportunityOverlay(this.encounter.bus, (lines) => {
       this.overlayText.setText(lines.length > 0 ? lines : ['(no opportunities open)']);
     });
@@ -44,13 +52,29 @@ export class ArenaScene extends Phaser.Scene {
     this.keys = {
       light: keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.J),
       dodge: keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.K),
+      up: keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W),
+      down: keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S),
+      left: keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A),
+      right: keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D),
     };
     this.keys.light.on('down', () => this.encounter.player.tryLightAttack());
     this.keys.dodge.on('down', () => this.encounter.player.tryDodge());
+
+    this.cameras.main.setBounds(ARENA_BOUNDS.x, ARENA_BOUNDS.y, ARENA_BOUNDS.width, ARENA_BOUNDS.height);
+    this.cameras.main.startFollow(this.playerRect);
   }
 
   update(_time: number, delta: number): void {
+    const dx = (this.keys.right.isDown ? 1 : 0) - (this.keys.left.isDown ? 1 : 0);
+    const dy = (this.keys.down.isDown ? 1 : 0) - (this.keys.up.isDown ? 1 : 0);
+    this.encounter.setPlayerMoveInput(dx, dy);
+
     this.loop.advance(delta);
+
+    const playerPos = this.encounter.player.position;
+    const assaltantePos = this.encounter.assaltante.position;
+    this.playerRect.setPosition(playerPos.x, playerPos.y);
+    this.assaltanteRect.setPosition(assaltantePos.x, assaltantePos.y);
 
     this.playerRect.setFillStyle(this.encounter.player.isInvulnerable ? 0x8bc34a : 0x4caf50);
     this.assaltanteRect.setFillStyle(
